ead977c993256480cdc8dedfa374cc3fd432e4b1
JoshJon <joshua.j.jon@gmail.com>
2020-05-03 13:57:20 +1000

    Init hello world API

---
+++ dota_clarity/create_profile.py
@@ -0,0 +1,30 @@
+import json
+import boto3
+import logging
+import requests
+from requests_aws4auth import AWS4Auth
+
+"""
+Parameters
+----------
+event: dict, required
+    API Gateway Lambda Proxy Input Format
+    Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+context: object, required
+    Lambda Context runtime methods and attributes
+    Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+Returns
+------
+API Gateway Lambda Proxy Output Format: dict
+    Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+"""
+def lambda_handler(event, context):
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "Hello from Dota Clarity",
+            # "location": ip.text.replace("\n", "")
+        }),
+    }
